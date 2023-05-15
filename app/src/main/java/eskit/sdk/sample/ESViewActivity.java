370e6e4e69e2e3ce793742600fb511c808b8aac3
liulipeng <liulipeng@huan.tv>
2023-05-15 13:57:30 +0800

    ESSDK以View方式集成

---
+++ app/src/main/java/eskit/sdk/sample/ESViewActivity.java
@@ -0,0 +1,123 @@
+package eskit.sdk.sample;
+
+import static eskit.sdk.core.protocol.EsProtocolDispatcher.K_FROM;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import eskit.sdk.core.EsData;
+import eskit.sdk.core.EsManager;
+import eskit.sdk.core.count.BaseEvent;
+import eskit.sdk.core.internal.IEsActivityProxy;
+import eskit.sdk.support.EsException;
+import eskit.sdk.support.args.EsMap;
+
+public class ESViewActivity extends AppCompatActivity {
+
+    private final String ES_APP_PACKAGE = "es.extscreen.runtime";
+
+    private IEsActivityProxy mActivityProxy;
+
+    private FrameLayout rootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_es_view);
+
+        rootView = this.findViewById(R.id.es_root_view);
+
+        initES();
+    }
+
+    private void initES() {
+        EsData data = new EsData();
+        data.setAppPackage(ES_APP_PACKAGE);
+        EsMap exp = new EsMap();
+        EsMap from = new EsMap();
+        from.pushObject(BaseEvent.ES_REFERER, BaseEvent.FROM_OUTER);
+        from.pushObject(BaseEvent.ES_REFERER1, getPackageName());
+        from.pushObject(BaseEvent.ES_REFERER2, "ZeroPageActivity");
+        exp.pushMap(K_FROM, from);
+        data.setExp(exp);
+        data.isHomePage(true);
+
+        //
+        loadFromNetwork(data);
+    }
+
+    private void loadFromNetwork(EsData data) {
+        EsManager.get().load(this, data, new IEsActivityProxy.LoadCallback() {
+            @Override
+            public void onStartLoad(IEsActivityProxy proxy) {
+                mActivityProxy = proxy;
+            }
+
+            @Override
+            public void onEsViewCreated(View view) {
+                rootView.addView(view);
+            }
+
+            @Override
+            public void onError(EsException e) {
+            }
+
+            @Override
+            public void requestFinish() {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (mActivityProxy != null)
+            mActivityProxy.dispatchKeyEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mActivityProxy != null) {
+            if (mActivityProxy.onBackPressed(super::onBackPressed)) {
+                return;
+            }
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mActivityProxy != null) mActivityProxy.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mActivityProxy != null) mActivityProxy.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        if (mActivityProxy != null) mActivityProxy.onStart();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mActivityProxy != null) mActivityProxy.onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mActivityProxy != null) mActivityProxy.onDestroy();
+        mActivityProxy = null;
+        super.onDestroy();
+    }
+}
--- app/src/main/java/eskit/sdk/sample/MainActivity.java
+++ app/src/main/java/eskit/sdk/sample/MainActivity.java
@@ -1,6 +1,9 @@
 package eskit.sdk.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,34 @@ import eskit.sdk.core.EsManager;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private Button activityModeButton;
+    private Button viewModeButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startEsApp();
+        setContentView(R.layout.activity_main);
+
+        //
+        activityModeButton = this.findViewById(R.id.activity_mode_button);
+        activityModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startEsApp();
+            }
+        });
+
+        //
+        viewModeButton = this.findViewById(R.id.view_mode_button);
+        viewModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, ESViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void startEsApp() {
@@ -30,6 +57,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        finish();
     }
 }
